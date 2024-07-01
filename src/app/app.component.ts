e295fbefe61e19e562e16db149d16f2a0bf2ef04
Luz <cabiaadrianol@gmail.com>
2024-07-01 11:10:05 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'GestionAsesoria';
+  title = 'Gestion Asesoria Unas';
+  protected openLanguague: boolean = false;
 }
